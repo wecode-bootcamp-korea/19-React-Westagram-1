204fd981fea9138c228e929e5e729cdb1999f84d
hello-dodohee <73160509+hello-dodohee@users.noreply.github.com>
2021-04-05 20:38:07 +0900

    Modify login.js 리팩토링

--- src/pages/doheekim/Login/Login.js
+++ src/pages/doheekim/Login/Login.js
@@ -1,52 +1,70 @@
-import { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { withRouter } from 'react-router-dom';
-import './Login.scss';
+import { Component } from "react";
+import { Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
+import "./Login.scss";
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: "",
+      pw: "",
+    };
+  }
 
-    constructor() {
-        super();
-        this.state = {
-            backgroundColor: '#0094f64b',
-            id: "",
-            pw: "",
-            buttonOn:false
-        };
-    }
+  goToMain = () => {
+    this.props.history.push("/maindh");
+  };
 
-    goToMain = () => {
-        this.props.history.push('/maindh')
-    }
+  handleIdInput = (event) => {
+    this.setState({
+      id: event.target.value,
+    });
+  };
 
-    handleIdInput = (event) => {
-        this.setState({
-            id: event.target.value,
-        })
-    }
+  handlePwInput = (event) => {
+    this.setState({
+      pw: event.target.value,
+    });
+  };
 
-    handlePwInput = (event) => {
-        this.setState({
-            pw: event.target.value
-        })
-    }
+  btnChangeColor = () => {
+    return this.state.id.includes("@") && this.state.pw.length >= 5
+      ? "on"
+      : "off";
+  };
 
-    render() { 
-
-        return ( 
-            <>
-                <div className="login-wrap">
-                    <h1 className="title">Westagram</h1>
-                    <div className="inner-wrap">
-                        <input className="id" type="text" placeholder="전화번호, 사용자 이름 또는 이메일" onChange={this.handleIdInput} value={this.state.id}/>
-                        <input className="password" type="password" placeholder="비밀번호" onChange={this.handlePwInput} value={this.state.pw}/>
-                        <button style={{ backgroundColor: this.state.id.includes("@") && this.state.pw.length >= 5 ? '#0094f6' : this.state.backgroundColor}} onClick={this.goToMain}>로그인</button>
-                        <p><Link to="/Main">비밀번호를 잊으셨나요?</Link></p>
-                    </div>
-                </div>  
-            </>
-         );
-    }
+  render() {
+    return (
+      <>
+        <div className="login-wrap">
+          <h1 className="title">Westagram</h1>
+          <div className="inner-wrap">
+            <input
+              className="id"
+              type="text"
+              placeholder="전화번호, 사용자 이름 또는 이메일"
+              onChange={this.handleIdInput}
+              value={this.state.id}
+            />
+            <input
+              className="password"
+              type="password"
+              placeholder="비밀번호"
+              onChange={this.handlePwInput}
+              value={this.state.pw}
+            />
+            <button className={this.btnChangeColor()} onClick={this.goToMain}>
+              로그인
+            </button>
+            <p>
+              <Link to="/Main">비밀번호를 잊으셨나요?</Link>
+            </p>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default withRouter(Login);
--- src/pages/doheekim/Main/Component/Mainleft/Comments.js
+++ src/pages/doheekim/Main/Component/Mainleft/Comments.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+// import { IoMdCloseCircleOutline } from "react-icons/io";
+// import { FiHeart } from "react-icons/fi";
 class Comments extends React.Component {
   render() {
     const { userId, comment } = this.props;
@@ -7,6 +8,12 @@ class Comments extends React.Component {
       <li key={this.props.key}>
         <span className="chat-id">{userId}</span>
         <span className="chat-content">{comment}</span>
+        {/* <span className="chat-heart">
+          <FiHeart />
+        </span>
+        <span className="chat-remove" onClick={this.handleRemove}>
+          <IoMdCloseCircleOutline />
+        </span> */}
       </li>
     );
   }
